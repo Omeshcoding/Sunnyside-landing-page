b9730a85218979391f6ee7ae815566121cec2c82
umesh <umesharma.us11@gmail.com>
2022-09-18 21:01:18 +0530

    mobile menu completed and styles added to it

---
+++ app.js
@@ -0,0 +1,33 @@
+const navMenu = document.getElementById('nav');
+const menuBar = document.getElementById('menu-bars');
+
+// function toogleNav() {
+//   menuBar.classList.toogle('change');
+//   console.log('hey');
+//   navMenu.classList.toogle('active');
+//   if (navMenu.classList.contains('active')) {
+//     navMenu.classList.replace('overlay-left', 'overlay-right');
+//   } else {
+//     navMenu.classList.replace('overlay-right', 'overlay-left');
+//   }
+// }
+// menuBar.addEventListener('click', toogleNav);
+function toggleNav() {
+  // Toggle: Menu Bars Open/Closed
+  menuBar.classList.toggle('change');
+  // Toggle: Menu Active
+  navMenu.classList.toggle('overlay-active');
+  if (navMenu.classList.contains('overlay-active')) {
+    navMenu.classList.replace('overlay-slide-left', 'overlay-slide-right');
+
+    // body.style.backgroundColor = 'rgba(0,0,0,0.6)';
+
+    // Animate-In - Nav Items
+  } else {
+    navMenu.classList.replace('overlay-slide-right', 'overlay-slide-left');
+
+    // body.style.backgroundColor = '#fff';
+  }
+}
+// Event Listeners
+menuBar.addEventListener('click', toggleNav);
